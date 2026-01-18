bf7a16fee2aba9e440480655a44b8fc438ae8c97
th3shiva <sharmasandeep1337@gmail.com>
2026-01-18 12:46:44 +0530

    last ques

---
+++ week-1-basics/Q Find the location.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    if(x==0 && y==0){
+        cout<<"Origin";
+    }else if(y==0 && x!=0){
+        cout<<"X axis";
+    }else if(x==0 && y!=0){
+        cout<<"Y axis";
+    }else if(x>0 && y>0){
+        cout<<"1st Quadrant";
+    }else if(x<0 && y>0){
+        cout<<"2nd Quadrant";
+    }else if(x<0 && y<0){
+        cout<<"3rd Quadrant";
+    }else if(x>0 && y<0){
+        cout<<"4th Quadrant";
+    }
+}
