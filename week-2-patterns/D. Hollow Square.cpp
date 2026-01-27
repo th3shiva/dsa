fb530f8476aa1daa9250d1e4b76633ee30792ef8
th3shiva <sharmasandeep1337@gmail.com>
2026-01-27 07:45:24 +0530

    pattern b,c and d

---
+++ week-2-patterns/D. Hollow Square.cpp
@@ -0,0 +1,27 @@
+// InputCopy
+// 6
+// OutputCopy
+// ******
+// *    *
+// *    *
+// *    *
+// *    *
+// ******
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            
+             if(i==0 or i==n-1 or j==0 or j==n-1){
+            cout<<"*";
+        }else{
+               cout<<" ";
+        }
+    }
+    cout<<endl;
+}
+    return 0;
+}
