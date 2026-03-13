2e300786c376a616751d61503cd19fc257fde4dd
th3shiva <sharmasandeep1337@gmail.com>
2026-03-13 08:09:05 +0530

    wave-2 2d arrays

---
+++ week-5-2d/I - Wave - II.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int a,b;
+	cin>>a>>b;
+	int arr[a][b];
+	for(int i=0;i<a;i++){
+	    for(int j=0;j<b;j++){
+	        cin>>arr[i][j];
+	    }
+	}
+	
+	
+	for(int j=0;j<b;j++){
+	   
+	       // cout<<i<<j<<endl;
+	       // cout<<arr[i][j]<<" ";
+	       if(j%2==0){
+	            for(int i=0;i<a;i++){
+	           cout<<arr[i][j]<<" ";
+	       }
+	    }else{
+	        for(int i=a-1;i>=0;i--){
+	             cout<<arr[i][j]<<" ";
+	        }
+	    }
+	}
+ 
+}
