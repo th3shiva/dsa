67f035e69dcac62a1ce5c33ef14a6b47ff198388
th3shiva <sharmasandeep1337@gmail.com>
2026-03-16 07:47:29 +0530

    2d complete

---
+++ week-5-2d/G - Row with max 1s.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m,n;
+    cin>>m>>n;
+    int arr[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int maxCount=0;
+    int bestRow=-1;
+     for(int i=0;i<m;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==1){
+                count++;
+            }
+            
+        if(maxCount<count){
+            maxCount=count;
+            bestRow=i;
+        }
+        }
+    }
+    cout<<bestRow;
+
+}
